ea9106581dad45729cbf2bac13a04b3371e730df
Chinmay <catrawalkar@gmail.com>
2019-08-30 10:00:19 +0530

    Updates

---
+++ precache-manifest.7580a89ab089cfdd0827ecb936a97d64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8bcdf64525f8ecc88093304c62cecf",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9e7a712a2fab5d74f3b5",
+    "url": "/portfolio/static/css/main.5fd1bf06.chunk.css"
+  },
+  {
+    "revision": "ed4daba394cd7d0ed28e",
+    "url": "/portfolio/static/js/2.fa5bf72f.chunk.js"
+  },
+  {
+    "revision": "9e7a712a2fab5d74f3b5",
+    "url": "/portfolio/static/js/main.c4f926d2.chunk.js"
+  },
+  {
+    "revision": "d6ea29ff0768a57ccac5",
+    "url": "/portfolio/static/js/runtime~main.ecf29490.js"
+  }
+]);
